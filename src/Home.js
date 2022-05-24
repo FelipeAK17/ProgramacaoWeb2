9f5677b52f04cccd001879f4a974a1fbef0441c5
FelipeAK17 <kleinandrefelipe@gmail.com>
2022-05-23 22:24:20 -0300

    a

---
+++ src/Home.js
@@ -0,0 +1,41 @@
+import './Home.css';
+
+
+function Home() {
+    return <h1>Home</h1>
+}
+
+export function Teste() {
+    return <h1>Teste</h1>
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default Home;
